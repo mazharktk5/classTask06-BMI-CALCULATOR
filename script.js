da864c3d0f06836531587d2cd5c0a02e10a693c9
https://github.com/mazharktk5 <mazharahmad862@gmail.com>
2024-10-06 05:56:15 -0700

    added div for bmi

--- script.js
+++ script.js
@@ -2,13 +2,25 @@
 var button = document.getElementById("btn")
 
 function calculateBMI(){
+    
     var weight = parseFloat(document.getElementById("weight").value)
     var height = parseFloat(document.getElementById("height").value)
+
+    
     
     var bmi = weight / (height * height)
     // document.getElementById("bmi").innerHTML = "Your BMI is: " + bmi.toFixed(2)
     
-    if(bmi < 18.5){
+// check if the weight and height is entered in negative or left empty then show alert.
+if (weight <= 0 || height <= 0) {
+    alert("Please enter valid weight and height.")
+    return;
+    } else {
+        document.getElementById("bmi").innerHTML = "Your BMI is: " + bmi.toFixed(2)
+    }
+
+
+     if(bmi < 18.5){
         document.getElementById("bmi-result").innerHTML = "Underweight"
     } else if(bmi >= 18.5 && bmi <= 24.9){
         document.getElementById("bmi-result").innerHTML = "Normal weight"
@@ -17,5 +29,8 @@ function calculateBMI(){
     } else {
         document.getElementById("bmi-result").innerHTML = "Obese"
     }
+
+    weight.value = " "
+    height.value = " "
     
     }
