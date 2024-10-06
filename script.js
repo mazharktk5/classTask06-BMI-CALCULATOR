31fe0dc7fd74ee262a25451488af209fcffe67e5
https://github.com/mazharktk5 <mazharahmad862@gmail.com>
2024-10-06 05:40:29 -0700

    added conditions for bmi

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ function calculateBMI(){
     var height = parseFloat(document.getElementById("height").value)
     
     var bmi = weight / (height * height)
-    document.getElementById("bmi").innerHTML = "Your BMI is: " + bmi.toFixed(2)
+    // document.getElementById("bmi").innerHTML = "Your BMI is: " + bmi.toFixed(2)
     
-}
+    if(bmi < 18.5){
+        document.getElementById("bmi-result").innerHTML = "Underweight"
+    } else if(bmi >= 18.5 && bmi <= 24.9){
+        document.getElementById("bmi-result").innerHTML = "Normal weight"
+    } else if(bmi >= 25 && bmi <= 29.9){
+        document.getElementById("bmi-result").innerHTML = "Overweight"
+    } else {
+        document.getElementById("bmi-result").innerHTML = "Obese"
+    }
+    
+    }
